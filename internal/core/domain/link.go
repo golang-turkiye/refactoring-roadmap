375fd77a373a7666a59c1f2e7c7bada189754c2d
Alameddin Çelik <alameddinc@gmail.com>
2022-08-06 19:51:03 +0300

    Step 6: Domain Updated

--- internal/core/domain/link.go
+++ internal/core/domain/link.go
@@ -4,9 +4,9 @@ import "gorm.io/gorm"
 
 type Link struct {
 	gorm.Model
-	LongUrl        string `json:"long_url"`
-	ShortenURLPath string `json:"shorten_url_path"`
-	IsDeleted      bool   `json:"is_deleted"`
-	OwnerID        uint   `json:"owner_id"`
-	User           User   `json:"-" gorm:"foreignKey:OwnerID"`
+	LongUrl   string `json:"long_url"`
+	ShortUrl  string `json:"short_url"`
+	IsDeleted bool   `json:"is_deleted"`
+	OwnerID   uint   `json:"owner_id"`
+	User      User   `json:"-" gorm:"foreignKey:OwnerID"`
 }
