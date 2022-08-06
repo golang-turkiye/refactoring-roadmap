17f8ba24457820569b32b0370123fac1efcd01dd
Alameddin Çelik <alameddinc@gmail.com>
2022-08-06 15:55:25 +0300

    executable file created

--- cmd/single/main.go
+++ cmd/single/main.go
@@ -1,4 +1,36 @@
 package main
 
+import (
+	"github.com/Golang-Turkiye/refactoring-roadmap/internal/core/handler/apiV1"
+	"github.com/Golang-Turkiye/refactoring-roadmap/internal/core/repository/postgres"
+	v1 "github.com/Golang-Turkiye/refactoring-roadmap/internal/core/service/v1"
+	"github.com/Golang-Turkiye/refactoring-roadmap/pkg/database/localDB"
+	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
+)
+
 func main() {
+	db, err := localDB.Connection("mock_db.db")
+	if err != nil {
+		panic(err)
+	}
+
+	// Router & Logger Generated
+	router := mux.NewRouter()
+	logger := logrus.New()
+	// Repositories Generated
+	userRepo, err := postgres.NewUserRepository(db, logger)
+	if err != nil {
+		panic(err)
+	}
+	linkRepo, err := postgres.NewLinkRepository(db, logger)
+	if err != nil {
+		panic(err)
+	}
+	// Services Generated
+	linkService := v1.NewLinkService(linkRepo)
+	userService := v1.NewUserService(userRepo)
+	// Handlers Generated
+	handler := apiV1.NewShortURLHander(linkService, userService, router, logger)
+	handler.Run()
 }
