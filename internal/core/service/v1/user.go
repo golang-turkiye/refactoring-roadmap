0c9f4725cc35394ebebdef505ba77651dda734f5
Alameddin Çelik <alameddinc@gmail.com>
2022-08-06 18:01:16 +0300

    Step 5.6: User Tests Completed

--- internal/core/service/v1/user.go
+++ internal/core/service/v1/user.go
@@ -3,6 +3,7 @@ package v1
 import (
 	"github.com/Golang-Turkiye/refactoring-roadmap/internal/core/domain"
 	"github.com/Golang-Turkiye/refactoring-roadmap/internal/core/repository"
+	"github.com/Golang-Turkiye/refactoring-roadmap/pkg/authentication"
 )
 
 type UserService struct {
@@ -16,12 +17,21 @@ func NewUserService(userRepository repository.UserRepository) *UserService {
 	}
 }
 
-func (s *UserService) Login(*domain.User) (string, error) {
-	return "", nil
+// Login is used to login a user.
+func (s *UserService) Login(user *domain.User) (string, error) {
+	token, err := authentication.GenerateToken(user.Email)
+	if err != nil {
+		return "", err
+	}
+	return token, nil
 }
+
+// GetUser is used to get a user by id.
 func (s *UserService) GetUser(userID uint) (*domain.User, error) {
-	return nil, nil
+	return s.userRepository.GetUserByID(userID)
 }
+
+// GetUserByEmail is used to get a user by email.
 func (s *UserService) GetUserByEmail(email string) (*domain.User, error) {
-	return nil, nil
+	return s.userRepository.GetUserByEmail(email)
 }
--- internal/core/service/v1/user_test.go
+++ internal/core/service/v1/user_test.go
@@ -1,1 +1,106 @@
 package v1_test
+
+import (
+	"github.com/Golang-Turkiye/refactoring-roadmap/internal/core/domain"
+	"github.com/Golang-Turkiye/refactoring-roadmap/internal/core/repository/mocks"
+	v1 "github.com/Golang-Turkiye/refactoring-roadmap/internal/core/service/v1"
+	"github.com/stretchr/testify/assert"
+	"gorm.io/gorm"
+	"testing"
+)
+
+func TestNewUserService(t *testing.T) {
+	userRepo := new(mocks.MockUserRepository)
+	userService := v1.NewUserService(userRepo)
+	assert.NotNil(t, userService)
+}
+
+func TestUserService_GetUser(t *testing.T) {
+	userRepo := new(mocks.MockUserRepository)
+	userService := v1.NewUserService(userRepo)
+	assert.NotNil(t, userService)
+	testCases := []struct {
+		name     string
+		user     *domain.User
+		expected error
+	}{
+		{
+			name: "Success",
+			user: &domain.User{
+				Model: gorm.Model{
+					ID: 1,
+				},
+				Email:    "alameddinc+test@gmail.com",
+				Password: "123456",
+			},
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			userRepo.On("GetUserByID", tc.user.ID).Return(tc.user, tc.expected)
+			user, err := userService.GetUser(tc.user.ID)
+			assert.Equal(t, err, tc.expected)
+			assert.Equal(t, user, tc.user)
+		})
+	}
+}
+
+func TestUserService_GetUserByEmail(t *testing.T) {
+	userRepo := new(mocks.MockUserRepository)
+	userService := v1.NewUserService(userRepo)
+	assert.NotNil(t, userService)
+	testCases := []struct {
+		name     string
+		user     *domain.User
+		expected error
+	}{
+		{
+			name: "Success",
+			user: &domain.User{
+				Model: gorm.Model{
+					ID: 1,
+				},
+				Email:    "alameddinc+test@gmail.com",
+				Password: "123456",
+			},
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			userRepo.On("GetUserByEmail", tc.user.Email).Return(tc.user, tc.expected)
+			user, err := userService.GetUserByEmail(tc.user.Email)
+			assert.Equal(t, err, tc.expected)
+			assert.Equal(t, user, tc.user)
+		})
+	}
+}
+
+func TestUserService_Login(t *testing.T) {
+	userRepo := new(mocks.MockUserRepository)
+	userService := v1.NewUserService(userRepo)
+	assert.NotNil(t, userService)
+	testCases := []struct {
+		name     string
+		user     *domain.User
+		expected error
+	}{
+		{
+			name: "Success",
+			user: &domain.User{
+				Model: gorm.Model{
+					ID: 1,
+				},
+				Email:    "alameddinc+test@gmail.com",
+				Password: "123456",
+			},
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			userRepo.On("GetUserByEmail", tc.user.Email).Return(tc.user, tc.expected)
+			token, err := userService.Login(tc.user)
+			assert.Equal(t, err, tc.expected)
+			assert.Equal(t, token, "")
+		})
+	}
+}
