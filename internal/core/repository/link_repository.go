916fe2c61f59b373906b7b56585f8ad79df6bb8a
Alameddin Çelik <alameddinc@gmail.com>
2022-08-06 11:58:58 +0300

    Updated Method Comments

--- internal/core/repository/link_repository.go
+++ internal/core/repository/link_repository.go
@@ -2,6 +2,7 @@ package repository
 
 import "github.com/Golang-Turkiye/refactoring-roadmap/internal/core/domain"
 
+// LinkRepository is the repository for links.
 type LinkRepository interface {
 	GetLinkByID(linkID uint) (*domain.Link, error)
 	GetLinkByURL(url string) (*domain.Link, error)
--- internal/core/repository/user_repository.go
+++ internal/core/repository/user_repository.go
@@ -2,6 +2,7 @@ package repository
 
 import "github.com/Golang-Turkiye/refactoring-roadmap/internal/core/domain"
 
+// UserRepository is the repository for users.
 type UserRepository interface {
 	GetUserByID(userID uint) (*domain.User, error)
 	GetUserByEmail(email string) (*domain.User, error)
